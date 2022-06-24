b737712dbd1197af404122e71ff87fc424657522
Amagrey <105565896+Amagrey@users.noreply.github.com>
2022-06-24 09:03:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var a =parseInt (prompt('Enter first number'));
+var operator = prompt('Enter Operator +, -, *, /');
+var b  =parseInt (prompt('Enter second number'));
+
+
+ if (operator == '+' )
+     {
+    alert(a + b);
+}else if (operator == '-' )
+ {
+    alert(a - b);
+}else if (operator == '*' ) 
+{
+    alert(a * b);
+}else if (operator == '/' ) {
+    alert(a / b);
+};
+
+
+
